030baad1657940c1742e259d596569f61661aa17
slavstepanyan <78844984+slavstepanyan@users.noreply.github.com>
2021-04-09 10:22:22 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int max(int num1, int num2)
+{
+	int temp = -1*(b)/a
+        return temp;
+}
+
+int main()
+{
+        int a = 0;
+        int b = 0;
+        std::cout << "Enter first value" << std::endl;
+        std::cin >> a;
+        if (std::cin.fail()) {
+                std::cout << "First a of the function" << std::endl;
+                return EXIT_FAILURE;
+        }
+        std::cout << "Enter b of the function" << std::endl;
+        std::cin >> b;
+        if (std::cin.fail()) {
+                std::cout << "Second is not a number" << std::endl;
+                return EXIT_FAILURE;
+        }
+        int x = value(a, b);
+        std::cout << "x is " << x << std::endl;
+        return EXIT_SUCCESS;
+}
